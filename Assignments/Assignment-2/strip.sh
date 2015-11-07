7e1d643f715a0df4cc94ecfc2a59a7d3f00bd749
venujan99 <venujan99@github.com>
2015-11-06 22:18:53 -0500

    Assignment 2: completed strip.sh

---
+++ Assignments/Assignment-2/strip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Name: Venujan Satchithanantham   Student ID: 100600999
+
+#arguements to pass 
+cat $3 | tail -n +$1 | head -n -$2 
--- Midterm/strip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Venujan Satchithanantham 100600999
-head -n k gadsby.txt | tail -n m > gadsby_stripped.txt
-
